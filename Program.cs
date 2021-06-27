e8b86c4019c5b4c893124d9400e7b68a999a1d71
hernantorres <hernan.torres@ucr.ac.cr>
2021-06-27 11:16:54 -0600

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tarea8PKI 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Selecionar accion
+            
+            
+            // 1.	Hacer un proceso de autenticación simple mediante certificado digital.
+
+
+
+            
+        }
+    }
+}
