730f754b8f53c5c96146ff577f505d77bee7cb3b
hernantorres <hernan.torres@ucr.ac.cr>
2021-07-03 22:40:03 -0600

    66%

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using Org.BouncyCastle.Asn1.Pkcs;
 using Org.BouncyCastle.Asn1.X509;
 using Org.BouncyCastle.Cms;
 using Org.BouncyCastle.Crypto;
@@ -11,6 +12,7 @@ using Org.BouncyCastle.Crypto.Encodings;
 using Org.BouncyCastle.Crypto.Engines;
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Pkix;
+using Org.BouncyCastle.Security;
 using Org.BouncyCastle.Utilities.Collections;
 using Org.BouncyCastle.X509;
 using Org.BouncyCastle.X509.Store;
@@ -19,46 +21,183 @@ namespace Tarea8PKI
 {
     class Program
     {
-        static List<X509Certificate> parents;
+        // Certificados de CA Raiz y Emisora
+        static List<X509Certificate> parentsCerts;
+        static List<X509Crl> parentsCrls;
+        static List<X509Certificate> usersCerts;
+        static string tokenTruth = "PKI2020";
         static string rutaCertRaiz = @"C:\Users\User\Desktop\5to año compu\llave pública\mi certificado\CA-RAIZ_RAIZ-CA.crt";
         static string rutaCertEmisora = @"C:\Users\User\Desktop\5to año compu\llave pública\mi certificado\EMISORA.coviticos.pki_coviticos-EMISORA-CA.cer";
         static string rutaCrlRaiz = @"C:\Users\User\Desktop\5to año compu\llave pública\mi certificado\RAIZ-CA.crl";
         static string rutaCrlEmisora = @"C:\Users\User\Desktop\5to año compu\llave pública\mi certificado\coviticos-EMISORA-CA.crl";
 
+
         static void Main(string[] args)
         {  
             
-
             Console.WriteLine("PKI - Tarea 8");
-            Console.WriteLine("Cargando certificados y CRLs de CA raiz y emisora...");
+            Console.WriteLine("Cargando certificados y CRLs...");
+            parentsCerts = loadParentsCerts(rutaCertRaiz, rutaCertEmisora);
+            parentsCrls = loadParentsCrls(rutaCrlRaiz, rutaCrlEmisora);
 
-            parents = loadRootAndIssuer(rutaCertRaiz, rutaCertEmisora);
+            // Solo contamos con un usuario
+            Console.WriteLine("Cargando usuarios...");
+            usersCerts = new  List<X509Certificate>();
+            usersCerts.Add(LoadCertificate(@"C:\Users\User\Desktop\5to año compu\llave pública\mi certificado\certificado.cer"));
             
+            bool correct = true;
+
             // Selecionar accion
             Console.WriteLine("Seleccione una opcion con el respectivo numero:");
             Console.WriteLine("1.	Hacer un proceso de autenticación simple mediante certificado digital.");
             Console.WriteLine("2.	Generar una firma digital (PKCS#7/CMS).");
             Console.WriteLine("3.	Solicitar una estampa de tiempo a la TSA SINPE.");
             Console.WriteLine("4.	Test de llaves.");
+            Console.WriteLine("5.	Salir.");
+            Console.WriteLine();
 
             char inputChar = Console.ReadKey().KeyChar;
-            if(inputChar == '2')
+            
+
+            if(inputChar == '1')
             {
-                generateSign();
+                // Aca decide si quiere obtener un token de autenticacion o autenticarse
+                authenticateViaCertificate(); 
+            }
+            else if(inputChar == '2')
+            {
+                generateCmsSign();
                 
             }
-        
-            if(inputChar == '4')
+            else if(inputChar == '3')
+            {
+                // sinpeTimeStamp();
+            }
+            else if(inputChar == '4')
             {
                 keysTest();
             }
-            
-            // 1.	Hacer un proceso de autenticación simple mediante certificado digital.
-
-            
+            else if(inputChar == '5')
+            {
+                correct = false;
+            }
+            else
+            {
+                Console.WriteLine("Opccion incorrecta");
+            }    
         }
 
-        static void generateSign()
+        static void authenticateViaCertificate()
+        {
+            bool correct = true;
+
+            Console.WriteLine("Seleccione una opcion con el respectivo numero:");
+            Console.WriteLine("1.	Obtener un token de autenticacion.");
+            Console.WriteLine("2.	Autenticarse.");
+            Console.WriteLine("3.	Volver.");
+
+            char inputChar = Console.ReadKey().KeyChar;
+            
+            if(inputChar == '1')
+            {
+                generateAuthToken();
+            }
+            else if(inputChar == '2')
+            {
+                authenticateCertAndToken();
+                
+            }
+            else if(inputChar == '5')
+            {
+                correct = false;
+            }
+            else
+            {
+                Console.WriteLine("Opccion incorrecta");
+            }
+
+
+        } 
+
+        static void generateAuthToken()
+        {
+            Console.WriteLine("Generando token para HERNAN TORRES NUÑEZ");
+            string rutaLlavePrivada = @"C:\Users\User\Desktop\5to año compu\llave pública\mi certificado\key.pem";
+            RsaKeyParameters PrivateKey = (RsaKeyParameters)ReadAsymmetricKeyParameter(rutaLlavePrivada);
+            byte[] token = GenerateSimpleSignature(tokenTruth, PrivateKey);
+
+            string tokenPath = @"C:\Users\User\Desktop\token";
+            File.WriteAllBytes(tokenPath, token);
+            Console.WriteLine("Token guardado en: C:\\Users\\User\\Desktop\\token");
+        }
+
+        static void authenticateCertAndToken()
+        {
+            Console.WriteLine("Para autenticarse provea los siguientes elementos:");
+            Console.WriteLine("Ruta de su certificado:");
+            string certPath = Console.ReadLine();
+            Console.WriteLine("Ruta de su token:");
+            string tokenPath = Console.ReadLine();
+
+            X509Certificate cert = LoadCertificate(certPath);
+            byte[] signature = File.ReadAllBytes(tokenPath);
+
+            // Construimos la cadena del certificado
+            Console.WriteLine("Construyendo cadena de certificados...");
+            BuildCertificateChain(cert, parentsCerts);
+
+            // Verificamos su estado en el CRL de la CA EMISORA
+            Console.WriteLine("Verificando estado del certificado...");
+            bool revocado = checkRevocation(cert,  parentsCrls[1]);
+
+            bool valido = ValidateSimpleSignature(tokenTruth, 
+                                                    signature, 
+                                                    (RsaKeyParameters)cert.GetPublicKey());
+            // Chequear que el token es valido y el certificado no esta revocado (la cadena falla al construirce)
+            if (valido && !revocado)
+            {
+                var name = new X509Name(cert.SubjectDN.ToString());
+                var nombre = name.GetValueList(X509Name.CN).OfType<string>().FirstOrDefault();
+                var cedula = name.GetValueList(X509Name.SerialNumber).OfType<string>().FirstOrDefault();
+                Console.WriteLine("Autenticado!");
+                Console.WriteLine("Usuario: " + nombre);
+                Console.WriteLine("Cédula: " + cedula);
+            }
+            else
+            {
+                Console.WriteLine("Fallo en autenticación, el certificado es el incorrecto, o no se reconoce su token.");
+            }
+
+
+
+        }
+
+        // https://gist.github.com/itsho/98bbb6d668b18072b6ffdf089ef28fc2
+        static bool ValidateSimpleSignature(string sourceData, byte[] signature, RsaKeyParameters publicKey)
+		{
+			byte[] tmpSource = Encoding.ASCII.GetBytes(sourceData);
+
+			ISigner signClientSide = SignerUtilities.GetSigner(PkcsObjectIdentifiers.Sha256WithRsaEncryption.Id);
+			signClientSide.Init(false, publicKey);
+			signClientSide.BlockUpdate(tmpSource, 0, tmpSource.Length);
+
+			return  signClientSide.VerifySignature(signature);
+		}
+
+        // https://gist.github.com/itsho/98bbb6d668b18072b6ffdf089ef28fc2
+		static byte[] GenerateSimpleSignature(string sourceData, RsaKeyParameters privateKey)
+		{
+			byte[] tmpSource = Encoding.ASCII.GetBytes(sourceData);
+
+			ISigner sign = SignerUtilities.GetSigner(PkcsObjectIdentifiers.Sha256WithRsaEncryption.Id);
+			sign.Init(true, privateKey);
+			sign.BlockUpdate(tmpSource, 0, tmpSource.Length);
+			return sign.GenerateSignature();
+		}
+
+    
+        // Genera la firma llamando a sign
+        static void generateCmsSign()
         {
             var path = @"C:\Users\User\Desktop\firmado.data";
             string text = "Este archivo esta firmado";
@@ -72,17 +211,13 @@ namespace Tarea8PKI
             X509Certificate certificado = LoadCertificate(rutaCertificado);
             RsaKeyParameters PublicKey = (RsaKeyParameters)certificado.GetPublicKey();
             RsaKeyParameters PrivateKey = (RsaKeyParameters)ReadAsymmetricKeyParameter(rutaLlavePrivada);
-            
-            
-            //X509Certificate prueba = parents[1];
-            //prueba.Verify(parents[0].GetPublicKey());
 
-            byte[] signedData = Sign(data, 
-                                PrivateKey, 
-                                certificado, 
-                                BuildCertificateChain(certificado, parents) );
+            byte[] signedData = cmsSign(data, //mensaje para crear firma
+                                PrivateKey, // llave privada
+                                certificado, // certificado de mi persona
+                                BuildCertificateChain(certificado, parentsCerts) ); // certificado de CA raiz y emisora
             File.WriteAllBytes(path, signedData);
-            Console.WriteLine("The data has been written to the file.");
+            Console.WriteLine("La firma se ha escrito en: C:\\Users\\User\\Desktop\\firmado.data");
         }
 
         static void keysTest()
@@ -135,6 +270,22 @@ namespace Tarea8PKI
             return cert;
         }
 
+        static X509Crl LoadCrl(string filename)
+        {
+            X509CrlParser certParser = new X509CrlParser();
+            FileStream fs = new FileStream(filename, FileMode.Open);
+            X509Crl crl = certParser.ReadCrl(fs);
+            fs.Close();
+
+            return crl;
+        }
+
+        static bool checkRevocation(X509Certificate cert, X509Crl crl)
+        {
+             bool result = crl.IsRevoked(cert);
+             return result;
+        }
+
         static AsymmetricKeyParameter ReadAsymmetricKeyParameter(string pemFilename)
         {
             var fileStream = System.IO.File.OpenText(pemFilename);
@@ -160,15 +311,25 @@ namespace Tarea8PKI
         }
 
         // Se manejan solo dos parametros por la simplicidad de la infraestructura  
-        static List<X509Certificate> loadRootAndIssuer(string rootPath, string issuerPath)
+        static List<X509Certificate> loadParentsCerts(string rootPath, string issuerPath)
         {
             List<X509Certificate> parents = new List<X509Certificate>();
             parents.Add(LoadCertificate(rootPath));
             parents.Add(LoadCertificate(issuerPath));
             return parents;
         }
+
+        static List<X509Crl> loadParentsCrls(string rootPath, string issuerPath)
+        {
+            List<X509Crl> parents = new List<X509Crl>();
+            parents.Add(LoadCrl(rootPath));
+            parents.Add(LoadCrl(issuerPath));
+            return parents;
+        }
              
         // https://stackoverflow.com/questions/10724594/build-certificate-chain-in-bouncycastle-in-c-sharp
+        // Lanza una excepcion si no se puede verificar la cadena
+        // ToDo: capturar excepcion
         static List<X509Certificate> BuildCertificateChain(X509Certificate primary, List<X509Certificate> additional)
         {
             X509CertificateParser parser = new X509CertificateParser();
@@ -195,7 +356,7 @@ namespace Tarea8PKI
             holder.Certificate = primary;
 
             // WITHOUT THIS LINE BUILDER CANNOT BEGIN BUILDING THE CHAIN
-        intermediateCerts.Add(holder.Certificate);
+            intermediateCerts.Add(holder.Certificate);
 
             PkixBuilderParameters builderParams = new PkixBuilderParameters(rootCerts, holder);
             builderParams.IsRevocationEnabled = false;
@@ -212,7 +373,7 @@ namespace Tarea8PKI
         }
 
         // https://csharp.hotexamples.com/site/file?hash=0x4aa3e7386cd74654bc3bcdb77943e7806d3f6142cc78f1c24a16806d30fd6add&fullName=csharp/tools/trust.bundler/FileSignerProvider.cs&project=DM-TOR/nhin-d
-        public static byte[] Sign(byte[] data, RsaKeyParameters key, X509Certificate signCert, List<X509Certificate> certChain)
+        public static byte[] cmsSign(byte[] data, RsaKeyParameters key, X509Certificate signCert, List<X509Certificate> certChain)
         {
             var generator = new CmsSignedDataGenerator();
             // Add signing key
